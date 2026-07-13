f63f2d9d34d6f193d9d2800b9d72d07b48133033
SuryaVarshney19 <surya.varshney2025@glbajajgroup.org>
2026-07-13 14:30:06 +0530

    day09 completed

---
+++ DAY09/Q_35.C
@@ -0,0 +1,20 @@
+/*Write a program to Print repeated character pattern.
+A
+BB
+CCC
+DDDD
+EEEEE
+*/
+#include <stdio.h>
+int main() {
+    int rows;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    for (int i = rows; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            printf("%c", 'A' + j - 1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
